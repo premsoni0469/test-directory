50c2cdec929257c55323308841b1073b98677bec
Prem <premsoni0469@gmail.com>
2024-07-23 10:53:13 +0530

    Modified Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Namaste Duniyaa!!!!");
+        int a = 5;
+        int b = 6;
+        int result = add(a, b);
+        System.out.println("Addition is "+ result);
     }
 }
+
+// Write code for add() method
